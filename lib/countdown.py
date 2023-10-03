bf63fa3817dac05ac0b6ac7bbacf43c3d3ded8cb
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-03 14:00:41 +0100

    One passed

--- lib/countdown.py
+++ lib/countdown.py
@@ -2,29 +2,30 @@
 
 import time
 
-def Run_count():
-    x = 1
-    while x < 10:
-        print(f"{x} is less than 10.")
-        x += 1
+# def Run_count():
+#     x = 1
+#     while x < 10:
+#         print(f"{x} is less than 10.")
+#         x += 1
 
-print(Run_count())
+# print(Run_count())
 
 
-def countdown():
-    y = 10
+def countdown(y):
+    
     while y > 0:
         print(f"{y} SECOND(S)!")
         y -= 1
     print(f"HAPPY NEW YEAR!")
     
-print(countdown())
+print(countdown(10))
 
-def countdown_with_sleep():
-    z = 10
-    while z > 0:
-        print(f"waiting for 1sec before {z} run!")
+def countdown_with_sleep(z):
+    while z >= 1:
+        print(f"{z} SECONDS(S)!")
         z -= 1
         time.sleep(1)
-    print(f"HAPPY NEW YEAR!")
-print(countdown_with_sleep())
+        
+        
+    print("HAPPY NEW YEAR!")
+print(countdown_with_sleep(10))
